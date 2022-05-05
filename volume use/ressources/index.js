9ff41aa4c0898bf06b8307292e2543f451c06105
Karlito <karlo02@live.fr>
2022-05-05 15:16:29 +0200

    Add files via upload

---
+++ volume use/ressources/index.js
@@ -0,0 +1,6 @@
+const http = require('http'); 
+const port = 3000; 
+const server = http.createServer((req, res) => { res.statusCode = 200; 
+    res.setHeader('Content-Type', 'text/plain'); 
+    res.end('I`m only here to be linted...\n'); }); 
+    server.listen(port, () => { console.log(`Server running at http://localhost:${port}/`); });
